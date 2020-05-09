8e933628b141cee930b193f3850561865c1f86f9
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-09 13:22:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,8 @@ def key_for_min_value(name_hash)
    if current_minimum_val > value
      current_minimum_val = value
      current_key_assoc_w_min_val = key
-   end
+    end
+  end
  end
-end
 current_key_assoc_w_min_val
+end
